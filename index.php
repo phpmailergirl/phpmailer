cc352c0fc1049020ac1be54df9fd3956ba587916
phpmailergirl <phpmailgirl@aol.com>
2016-07-22 18:21:39 -0400

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+$to = "somebody@example.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
+?>
+
 <!DOCTYPE html>
 <html>
 
